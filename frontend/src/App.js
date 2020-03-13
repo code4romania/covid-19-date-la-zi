2758100d2f13336aeb7c23c60852c7f33ac4c943
Andrei Cioban <andrei.cioban@assist.ro>
2020-03-13 23:24:56 +0200

    Initial commit + Bulma

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
+  );
+}
+
+export default App;
