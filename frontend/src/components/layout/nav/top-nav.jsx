f5ed1fa5c9bf4f0fb60883bbee47fa8f6ce8c63c
Cristi Habliuc <cristi.habliuc@gmail.com>
2020-03-14 14:26:57 +0200

    updated the app layout and a bit of CSS to resemble the Figma design

--- frontend/src/components/layout/nav/top-nav.jsx
+++ frontend/src/components/layout/nav/top-nav.jsx
@@ -5,37 +5,53 @@ import './top-nav.css';
 export class TopNav extends React.PureComponent {
   render() {
     return (
-      <nav
-        role="navigation"
-        aria-label="main navigation"
-        className="navbar border-bottom"
-      >
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item">
-            <strong>🏠 Co viz 19</strong>
-          </Link>
-          <button data-target="navMenu" className="button navbar-burger">
-            <span /> <span /> <span />
-          </button>
-        </div>
-        <div id="navMenu" className="navbar-menu">
-          <div className="navbar-start">
-            <Link to="/charts" className="navbar-item">
-              Charts
-            </Link>
-            <Link to="/not-charts" className="navbar-item">
-              Not charts
-            </Link>
-          </div>
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <Link to="/register" className="button is-primary">
-                SIGN UP
+      <div className="navigation-container">
+        <nav
+          role="navigation"
+          aria-label="main navigation"
+          className="navbar"
+        >
+          <div className="container border-bottom">
+            <div className="navbar-brand">
+              <Link to="/" className="navbar-item">
+                <img src="/images/logo-coviz.svg" className="logo-coviz" />
               </Link>
+              <button data-target="navMenu" className="button navbar-burger">
+                <span /> <span /> <span />
+              </button>
+            </div>
+            <div id="navMenu" className="navbar-menu">
+              <div className="navbar-start" />
+              <div className="navbar-end">
+                <Link to="/" className="navbar-item">
+                Home
+                </Link>
+                <Link to="/about" className="navbar-item">
+                Despre Proiect
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="level">
+            <div className="level-left" />
+            <div className="developer border-bottom level-right">
+              <span>Un proiect dezvoltat de</span>
+            &nbsp;&nbsp;
+              <a href="https://code4.ro" target="_blank">
+                <img src="/images/logo-code4.svg" alt="Code 4 Romania Logo" className="logo-code4" />
+              </a>
+            &nbsp;&nbsp;&nbsp;
+              <span>În parteneriat cu</span>
+            &nbsp;&nbsp;
+              <a href="https://www.gov.ro/" target="_blank">
+                <img src="/images/logo-gov.svg" height="30" alt="Guvernul Romaniei Logo" className="logo-gov" />
+              </a>
             </div>
           </div>
         </div>
-      </nav>
+      </div>
     );
   }
 }
