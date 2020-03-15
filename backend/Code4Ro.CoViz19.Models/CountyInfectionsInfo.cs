8b7d77917f3e7ac45ba02a39bdb12d9775602431
Ion Dormenco <idormenco@gmail.com>
2020-03-15 17:33:44 +0200

    make filese case sensitive

---
+++ backend/Code4Ro.CoViz19.Models/CountyInfectionsInfo.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Code4Ro.CoViz19.Models
+{
+    public class CountyInfectionsInfo
+    {
+        public string County { get; set; }
+        public int? NumberOfInfections { get; set; }
+    }
+}
---
+++ backend/Code4Ro.CoViz19.Models/ParsedDataModel.cs
@@ -0,0 +1,11 @@
+﻿using Code4Ro.CoViz19.Models;
+
+namespace Code4Ro.CoViz19.Models
+{
+    public class ParsedDataModel
+    {
+        public LiveUpdateData[] LiveUpdateData { get; set; }
+        public PatientInfo[] PatientsInfo { get; set; }
+        public CountyInfectionsInfo[] CountiesData { get; set; }
+    }
+}
