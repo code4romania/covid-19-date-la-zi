531128cb6f36027ffd393980e0a07faa4baf168f
Ion Dormenco <idormenco@gmail.com>
2020-03-16 23:32:02 +0200

    fix pr commets + update data

--- backend/Code4RO.CoViz19.Models/InfectionSourceType.cs
+++ backend/Code4RO.CoViz19.Models/InfectionSourceType.cs
@@ -1,9 +1,17 @@
-﻿namespace Code4Ro.CoViz19.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Code4Ro.CoViz19.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum InfectionSourceType
     {
+        [EnumMember(Value = "unknown")]
         Unknown = 1,
+        [EnumMember(Value = "intern")]
         Intern = 2,
+        [EnumMember(Value = "extern")]
         Extern = 3
     }
 }
