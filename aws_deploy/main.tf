d4fcb724e640f6213f5c6d014b2e75e3f09b8905
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2020-03-17 00:19:46 +0200

    Add remote tfstate

--- aws_deploy/main.tf
+++ aws_deploy/main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+terraform {
+  required_version = ">=0.12.13"
+  backend "s3" {
+    bucket         = "code4ro-terraform-tfstate"
+    key            = "date-la-zi/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
