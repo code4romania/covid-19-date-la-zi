dd290265e8035136b741aacc9b5df0ac97e693c0
Bogdan Vizureanu <bogdan.vizureanu@gmail.com>
2020-03-16 22:08:41 +0200

    Add local service for json data

---
+++ backend/Code4Ro.CoViz19.Api/Services/LocalDataProviderService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Code4Ro.CoViz19.Models;
+using Code4Ro.CoViz19.Services;
+using Newtonsoft.Json;
+
+namespace Code4Ro.CoViz19.Api.Services
+{
+    public class LocalDataProviderService : IDataProviderService
+    {
+        private readonly IFileService _fileService;
+        private ParsedDataModel _localData;
+
+        public LocalDataProviderService(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+
+        public async Task<ParsedDataModel> GetCurrentData() =>
+            _localData ??= JsonConvert.DeserializeObject<ParsedDataModel>(_fileService.GetRawData());
+    }
+
+}
