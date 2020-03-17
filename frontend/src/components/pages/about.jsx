d162b1858d5e3c9a533ca733fea5695c49bbaf59
Cristi Habliuc <cristi.habliuc@gmail.com>
2020-03-17 14:05:15 +0200

    replaced placeholders with live copy; removed shadows from cards, looks cleaner (#78)

--- frontend/src/components/pages/about.jsx
+++ frontend/src/components/pages/about.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { PageHeader } from '../layout/page.header';
 
 export class AboutPage extends React.PureComponent {
@@ -8,25 +9,31 @@ export class AboutPage extends React.PureComponent {
         <div className="container cards-row">
           <PageHeader
             title="Despre Proiect"
-            subtitle="Some Lorem ipsum about Date La Zi dolor sit amet, consectetur adipiscing elit. Elit,
-            duis pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium."
+
           />
         </div>
         <div className="container">
           <p>
-                Some More Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Elit, duis pretium.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium.
-                Some More Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Elit, duis pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Elit, duis pretium.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Elit, duis pretium.
+          COVID-19, o boală cauzată de o nouă tulpină de coronavirus identificată pentru prima dată în
+          decembrie 2019 in Wuhan, China, este una dintre cele mai grave crize de sănătate publică din
+          ultimele decenii. Dată fiind creșterea rapidă a cazurilor raportate în afara Chinei și a triplării
+          numărului de țări afectate, Organizația Mondială a Sănătății (OMS) a caracterizat COVID-19 ca o pandemie.
+          Potrivit OMS, dacă țările detectează, testează, tratează, izolează, urmăresc și își mobilizează
+          oamenii ca răspuns, pot dezvolta strategii de răspuns eficiente împotriva COVID-19.
           </p>
           <p>
-                Some More Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium.
+          Credem că este important ca publicul larg să aibă access la un instrument ușor de utilizat pentru
+          a urmări dimensiunea focarului pentru a vizualiza date corecte din surse
+          sigure. <Link to="/">Datelazi.ro</Link> prezintă infografii actualizeate periodic cu date anonimizate
+          colectate de Ministerul Sănătății și le transformă în informație grafică, ușor de înțeles pentru publicul
+          larg.
+          </p>
+          <p>
+            <Link to="/">Datelazi.ro</Link> este un  proiect realizat pro-bono de
+          voluntarii <a href="https://code4.ro">Code for Romania</a>, organizație
+          neguvernamentală independentă, neafiliată politic și apolitică, în cadrul Code for Romania Task Force
+          și în parteneriat cu Guvernul României prin Autoritatea pentru Digitalizarea României pe baza datelor
+          furnizate de Ministerul Sănătății.
           </p>
         </div>
       </section>
