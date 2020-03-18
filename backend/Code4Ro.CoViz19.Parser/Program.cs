ad91c7c22f4b281e43db6cee1f235aa1a9d88209
Bogdan Vizureanu <bogdan.vizureanu@gmail.com>
2020-03-19 00:34:59 +0200

    [parser] AddDockerSecrets

--- backend/Code4Ro.CoViz19.Parser/Program.cs
+++ backend/Code4Ro.CoViz19.Parser/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Code4Ro.CoViz19.Parser
 {
@@ -25,6 +20,7 @@ namespace Code4Ro.CoViz19.Parser
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     config.AddEnvironmentVariables();
+                    config.AddDockerSecrets();
                 });
     }
 }
