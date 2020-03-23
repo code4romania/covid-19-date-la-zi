0fb0f8387247af4f284c475be8bd4f93fd77190c
Ion Dormenco <idormenco@gmail.com>
2020-03-23 22:24:53 +0200

    implement parsing of age distribution

--- backend/Code4Ro.CoViz19.Parser/Parsers/PdfParser.cs
+++ backend/Code4Ro.CoViz19.Parser/Parsers/PdfParser.cs
@@ -11,34 +11,42 @@ namespace Code4Ro.CoViz19.Parser.Parsers
         {
             const string pattern = @"(\d+) cazuri diagnosticate covid";
 
-            var result = ExtractNumberByPattern(pattern, pdfContents);
+            var result = ExtractByPattern(pattern, pdfContents);
 
-            return result != null ? int.Parse(result) : -1;
-        }        
+            return StringToNumber(result);
+        }
 
         public static int ParseNumberCured(string pdfContents)
         {
             const string pattern = @"(\d+) cazuri vindecate";
 
-            var result = ExtractNumberByPattern(pattern, pdfContents);
+            var result = ExtractByPattern(pattern, pdfContents);
 
-            return result != null ? int.Parse(result) : -1;
+            return StringToNumber(result);
         }
 
         public static int ParseNumberDeceased(string pdfContents)
         {
             const string pattern = @"(\d+) decese";
 
-            var result = ExtractNumberByPattern(pattern, pdfContents);
+            var result = ExtractByPattern(pattern, pdfContents);
 
-            return result != null ? int.Parse(result) : -1;
+            return StringToNumber(result);
         }
 
         public static Dictionary<AgeRange, int> ParseDistributionByAge(string pdfContents)
         {
             return new Dictionary<AgeRange, int>()
             {
-                { AgeRange.Age09, 8}
+                { AgeRange.Age09, StringToNumber(ExtractByPattern(@"0-9\s*(\d+)\s*\(\d+%\)", pdfContents))},
+                { AgeRange.Age1019, StringToNumber(ExtractByPattern(@"10-19\s*(\d+)\s*\(\d+%\)", pdfContents) )},
+                { AgeRange.Age2029, StringToNumber(ExtractByPattern(@"20-29\s*(\d+)\s*\(\d+%\)", pdfContents) )},
+                { AgeRange.Age3039, StringToNumber(ExtractByPattern(@"30-39\s*(\d+)\s*\(\d+%\)", pdfContents) )},
+                { AgeRange.Age4049, StringToNumber(ExtractByPattern(@"40-49\s*(\d+)\s*\(\d+%\)", pdfContents) )},
+                { AgeRange.Age5059, StringToNumber(ExtractByPattern(@"50-59\s*(\d+)\s*\(\d+%\)", pdfContents) )},
+                { AgeRange.Age6069, StringToNumber(ExtractByPattern(@"60-69\s*(\d+)\s*\(\d+%\)", pdfContents) )},
+                { AgeRange.Age7079, StringToNumber(ExtractByPattern(@"70-79\s*(\d+)\s*\(\d+%\)", pdfContents) )},
+                { AgeRange.AgeOver80, StringToNumber(ExtractByPattern(@">80\s*(\d+)\s*\(\d+%\)", pdfContents) )},
             };
         }
 
@@ -46,12 +54,12 @@ namespace Code4Ro.CoViz19.Parser.Parsers
         {
             const string pattern = @"medie (\w+\s*\-\s*\w+|\w+) ani";
 
-            var result = ExtractNumberByPattern(pattern, pdfContents);
+            var result = ExtractByPattern(pattern, pdfContents);
 
             return result;
         }
 
-        private static string ExtractNumberByPattern(string pattern, string text)
+        private static string ExtractByPattern(string pattern, string text)
         {
             var regex = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
             var match = regex.Matches(text);
@@ -60,6 +68,11 @@ namespace Code4Ro.CoViz19.Parser.Parsers
                 return match[0].Groups[1].Value;
             }
             return null;
+        }
+
+        private static int StringToNumber(string input)
+        {
+            return !string.IsNullOrEmpty(input) ? int.Parse(input) : -1;
         }
     }
 }
