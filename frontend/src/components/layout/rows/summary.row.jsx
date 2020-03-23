7dac65ec510afbbc4f050735078ab32ed6bcb006
danut0nline <dan.grigore@hotmail.com>
2020-03-23 14:36:14 +0200

    Remove cards for hospitalized and ICU

--- frontend/src/components/layout/rows/summary.row.jsx
+++ frontend/src/components/layout/rows/summary.row.jsx
@@ -15,12 +15,8 @@ export class SummaryRow extends React.PureComponent {
       isLoaded: false,
       totalCases: 0,
       monitoredCases: 0,
-      hospitalizedCases: 0,
       curedCases: 0,
-      icuCases: 0,
       totalCasesHistory: [], // array of ints
-      hospitalizedCasesHistory: [], // array of ints
-      icuCasesHistory: [], // array of ints
       curedCasesHistory: [], // array of ints
     }
   }
@@ -46,12 +42,8 @@ export class SummaryRow extends React.PureComponent {
     const history = result.history
     const deathCasesHistory = history.map((entry) => { return entry.deaths || 0 })
     const totalCasesHistory = history.map((entry) => { return entry.confirmed || 0 })
-    const hospitalizedCasesHistory = history.map((entry) => { return entry.hospitalized || 0 })
-    const icuCasesHistory = history.map((entry) => { return entry.in_icu || 0 })
     const curedCasesHistory = history.map((entry) => { return entry.cured || 0 })
     const confirmed = summary.confirmed || 0
-    const hospitalized = summary.hospitalized || 0
-    const in_icu = summary.in_icu || 0
     const cured = summary.cured || 0
     const monitored = summary.monitored || 0
     const in_quarantine = summary.in_quarantine || 0
@@ -60,10 +52,6 @@ export class SummaryRow extends React.PureComponent {
       isLoaded: true,
       totalCases: confirmed.toLocaleString(),
       totalCasesHistory: totalCasesHistory,
-      hospitalizedCases: hospitalized.toLocaleString(),
-      hospitalizedCasesHistory: hospitalizedCasesHistory,
-      icuCases: in_icu.toLocaleString(),
-      icuCasesHistory: icuCasesHistory,
       curedCases: cured.toLocaleString(),
       curedCasesHistory: curedCasesHistory,
       monitoredCases: monitored.toLocaleString(),
@@ -135,25 +123,6 @@ export class SummaryRow extends React.PureComponent {
                 data={this.state.totalCasesHistory}
               />
             </div>
-            <div className="column">
-              <SummaryCard
-                to="/"
-                title="Cazuri spitalizate"
-                total={this.state.hospitalizedCases}
-                special={this.specialValueForHospitalized()}
-                data={this.state.hospitalizedCasesHistory}
-              />
-            </div>
-            <div className="column">
-              <SummaryCard
-                to="/"
-                title="Cazuri - terapie intensivă"
-                total={this.state.icuCases}
-                data={this.state.icuCasesHistory}
-              />
-            </div>
-          </div>
-          <div className="columns">
             <div className="column">
               <SummaryCard
                 to="/"
