22b948b156cd9ce3924eef5c5d169a175b0ba18f
decembrya <31412211+decembrya@users.noreply.github.com>
2020-03-23 21:27:01 +0200

    fix tests

--- backend/Code4Ro.CoViz19.Parser.UnitTests/PdfParserShould.cs
+++ backend/Code4Ro.CoViz19.Parser.UnitTests/PdfParserShould.cs
@@ -13,86 +13,51 @@ namespace Code4Ro.CoViz19.Parser.UnitTests
         private static Dictionary<string, string> _resourceDictionary = new Dictionary<string, string>();
 
         [Theory]
-        [MemberData(nameof(PdfInfectionsData))]
-        public void Parse_number_of_infections_correctly(string pdfText, int expectedMap)
+        [InlineData("TextInPdf_Coronavirus 17.03.pdf.txt", 217)]
+        [InlineData("TextInPdf_Coronavirus 18.03.pdf.txt", 260)]
+        [InlineData("TextInPdf_Coronavirus 19.03.pdf.txt", 277)]
+        [InlineData("TextInPdf_Coronavirus 20.03.pdf.txt", 308)]
+        [InlineData("TextInPdf_Coronavirus 21.03.pdf.txt", 367)]
+        [InlineData("TextInPdf_Coronavirus 22.03.pdf.txt", 433)]
+        [InlineData("TextInPdf_Coronavirus 23.03.pdf.txt", 576)]
+        public void Parse_number_of_infections_correctly(string file, int expectedMap)
         {
+            var pdfText = LoadResource(file);
             PdfParser.ParseNumberInfected(pdfText).ShouldBe(expectedMap);
-        }
-
-        public static IEnumerable<object[]> PdfInfectionsData()
-        {
-            return new (string pdfText, int number)[] {
-                (LoadResource("TextInPdf_Coronavirus 17.03.pdf.txt"), 25 ),
-                (LoadResource("TextInPdf_Coronavirus 18.03.pdf.txt"), 25 ),
-                (LoadResource("TextInPdf_Coronavirus 19.03.pdf.txt"), 25 ),
-                (LoadResource("TextInPdf_Coronavirus 20.03.pdf.txt"), 25 ),
-                (LoadResource("TextInPdf_Coronavirus 21.03.pdf.txt"), 25 ),
-                (LoadResource("TextInPdf_Coronavirus 22.03.pdf.txt"), 25 ),
-                (LoadResource("TextInPdf_Coronavirus 23.03.pdf.txt"), 25 ),
-            }.Select(t => new object[] { t.pdfText, t.number });
         }
 
         [Theory]
-        [MemberData(nameof(PdfCuredData))]
-        public void Parse_number_of_cured_correctly(string pdfText, int expectedMap)
+        [InlineData("TextInPdf_Coronavirus 17.03.pdf.txt", 19)]
+        [InlineData("TextInPdf_Coronavirus 18.03.pdf.txt", 19)]
+        [InlineData("TextInPdf_Coronavirus 19.03.pdf.txt", 25)]
+        [InlineData("TextInPdf_Coronavirus 20.03.pdf.txt", 31)]
+        [InlineData("TextInPdf_Coronavirus 21.03.pdf.txt", 52)]
+        [InlineData("TextInPdf_Coronavirus 22.03.pdf.txt", 64)]
+        [InlineData("TextInPdf_Coronavirus 23.03.pdf.txt", 73)]
+        public void Parse_number_of_cured_correctly(string file, int expectedMap)
         {
-            PdfParser.ParseNumberInfected(pdfText).ShouldBe(expectedMap);
-        }
-
-        public static IEnumerable<object[]> PdfCuredData()
-        {
-            return new (string pdfText, int number)[] {
-                (LoadResource("TextInPdf_Coronavirus 17.03.pdf.txt"), 35 ),
-                (LoadResource("TextInPdf_Coronavirus 18.03.pdf.txt"), 35 ),
-                (LoadResource("TextInPdf_Coronavirus 19.03.pdf.txt"), 35 ),
-                (LoadResource("TextInPdf_Coronavirus 20.03.pdf.txt"), 35 ),
-                (LoadResource("TextInPdf_Coronavirus 21.03.pdf.txt"), 35 ),
-                (LoadResource("TextInPdf_Coronavirus 22.03.pdf.txt"), 35 ),
-                (LoadResource("TextInPdf_Coronavirus 23.03.pdf.txt"), 35 ),
-            }.Select(t => new object[] { t.pdfText, t.number });
+            var pdfText = LoadResource(file);
+            PdfParser.ParseNumberCured(pdfText).ShouldBe(expectedMap);
         }
 
         [Theory]
-        [MemberData(nameof(PdfDeathData))]
-        public void Parse_number_of_deaths_correctly(string pdfText, int expectedMap)
+        [InlineData("TextInPdf_Coronavirus 17.03.pdf.txt", 0)]
+        [InlineData("TextInPdf_Coronavirus 18.03.pdf.txt", 0)]
+        [InlineData("TextInPdf_Coronavirus 19.03.pdf.txt", 0)]
+        [InlineData("TextInPdf_Coronavirus 20.03.pdf.txt", 0)]
+        [InlineData("TextInPdf_Coronavirus 21.03.pdf.txt", 0)]
+        [InlineData("TextInPdf_Coronavirus 22.03.pdf.txt", 2)]
+        [InlineData("TextInPdf_Coronavirus 23.03.pdf.txt", 4)]
+        public void Parse_number_of_deaths_correctly(string file, int expectedMap)
         {
-            PdfParser.ParseNumberInfected(pdfText).ShouldBe(expectedMap);
+            var pdfText = LoadResource(file);
+            PdfParser.ParseNumberDeceased(pdfText).ShouldBe(expectedMap);
         }
 
-        public static IEnumerable<object[]> PdfDeathData()
-        {
-            return new (string pdfText, int number)[] {
-                (LoadResource("TextInPdf_Coronavirus 17.03.pdf.txt"), 45 ),
-                (LoadResource("TextInPdf_Coronavirus 18.03.pdf.txt"), 45 ),
-                (LoadResource("TextInPdf_Coronavirus 19.03.pdf.txt"), 45 ),
-                (LoadResource("TextInPdf_Coronavirus 20.03.pdf.txt"), 45 ),
-                (LoadResource("TextInPdf_Coronavirus 21.03.pdf.txt"), 45 ),
-                (LoadResource("TextInPdf_Coronavirus 22.03.pdf.txt"), 45 ),
-                (LoadResource("TextInPdf_Coronavirus 23.03.pdf.txt"), 45 ),
-            }.Select(t => new object[] { t.pdfText, t.number });
-        }
 
-        private static string LoadResource(string resource)
-        {
-            if (_resourceDictionary.ContainsKey(resource))
-            {
-                return _resourceDictionary[resource];
-            }
-
-            var assembly = typeof(PdfParserShould).GetTypeInfo().Assembly;
-            var resourceKey = $"Code4Ro.CoViz19.Parser.UnitTests.PdfTexts.{resource}";
-            using (var stream = assembly.GetManifestResourceStream(resourceKey))
-            {
-                using (var sr = new StreamReader(stream))
-                {
-                    var content = sr.ReadToEnd();
-                    _resourceDictionary.Add(resource, content);
-
-                    return sr.ReadToEnd();
-                }
-            }
+        private  string LoadResource(string resource)
+        {           
+            return File.ReadAllText($"PdfTexts\\{resource}");
         }
     }
-
-
 }
