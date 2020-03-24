eb5db8786281b29f1222bdc1497ad7168058f6fc
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2020-03-24 22:43:28 +0200

    Split parser to it's own certificate & remove from output

--- aws_deploy/output.tf
+++ aws_deploy/output.tf
@@ -6,22 +6,10 @@ output "DNS_API" {
   value = "https://${module.api_dns.fqdn}"
 }
 
-output "DNS_Parser" {
-  value = "https://${module.parser_dns.fqdn}"
-}
-
 output "LB_FrontEnd" {
   value = module.front-end.dns
 }
 
 output "LB_API" {
   value = module.api.dns
-}
-
-output "LB_Parser" {
-  value = module.parser.dns
-}
-
-output "NAT_Egress_Elastic_IP" {
-  value = aws_eip.private.*.public_ip
 }
