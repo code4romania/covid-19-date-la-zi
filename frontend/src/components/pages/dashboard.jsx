e096c6ca8b927ebc7c04fc0cea08a3cdf4e6a1c4
catileptic <s.alexandra.stefanescu@gmail.com>
2020-03-25 23:13:07 +0200

    Update deceased (#222)

--- frontend/src/components/pages/dashboard.jsx
+++ frontend/src/components/pages/dashboard.jsx
@@ -37,7 +37,7 @@ export class Dashboard extends React.PureComponent {
 
           <SocialsShare currentPage="https://datelazi.ro/" />
 
-          <p>Date actualizate pe 25 Martie la 17:23.</p>
+          <p>Date actualizate pe 25 Martie la 22:48.</p>
         </div>
 
         <SummaryRow />
