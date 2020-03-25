c6aa79a61266b96cdff0aaeff58895430976e060
Ion Dormenco <idormenco@gmail.com>
2020-03-25 10:43:50 +0200

    Feature/implement quickstats (#183)
    * implement quickstats
    
    * because first record is a huge jump we will skip it
    
    * Update FrontendDataQueryHandler.cs
    
    * rename percentage property

--- backend/Code4Ro.CoViz19.Api/Handlers/FrontendDataQueryHandler.cs
+++ backend/Code4Ro.CoViz19.Api/Handlers/FrontendDataQueryHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Amazon.S3.Model;
 using Code4Ro.CoViz19.Api.Commands;
 using Code4Ro.CoViz19.Api.Commands.V2;
 using Code4Ro.CoViz19.Api.Models.V2;
@@ -10,13 +11,15 @@ using Code4Ro.CoViz19.Api.Services;
 using Code4Ro.CoViz19.Models.ParsedPdfModels;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob.Protocol;
 
 namespace Code4Ro.CoViz19.Api.Handlers
 {
     public class FrontendDataQueryHandler : IRequestHandler<GetDailyStatsV2, DailyStatsV2Model>,
         IRequestHandler<GetLatestDataV2, HistoricalPdfStats>,
         IRequestHandler<GetAgeHistogramV2, AgeHistogramV2Model>,
-        IRequestHandler<GetGenderStatsV2, GenderStatsV2Model>
+        IRequestHandler<GetGenderStatsV2, GenderStatsV2Model>,
+        IRequestHandler<GetQuickstatsV2Data, QuickStatsV2Model>
 
     {
         private readonly IDataProviderService _dataService;
@@ -40,11 +43,40 @@ namespace Code4Ro.CoViz19.Api.Handlers
                 CurrentDay = MapToDailyStats(currentData.CurrentDayStats)
             };
 
-            if (currentData?.HistoricalData != null)
+            if (currentData.HistoricalData != null)
             {
-                response.History = currentData.HistoricalData.Values.Select(MapToDailyStats).ToArray();
+                var mappedLists = currentData.HistoricalData.Values
+                    .OrderBy(x => x.ParsedOn)
+                    .Select(MapToDailyStats)
+                    .ToList();
+
+                mappedLists.Insert(0, new DailyStats());
+                mappedLists.Add(response.CurrentDay);
+
+                response.History = mappedLists
+                    .Select((el, i) => new { index = i, data = el })
+                    .Where(x => x.index > 0)
+                    .Select(x => GetDailyStatsDiff(x.data, mappedLists[x.index - 1]))
+                    .Skip(1)
+                    .ToArray();
             }
+
             return response;
+        }
+
+        private static DailyStats GetDailyStatsDiff(DailyStats currentDay, DailyStats previousDay)
+        {
+            var day = new DailyStats
+            {
+                Cured = currentDay.Cured - previousDay.Cured,
+                AverageAge = currentDay.AverageAge,
+                DatePublished = currentDay.DatePublished,
+                DatePublishedString = currentDay.DatePublishedString,
+                Deaths = currentDay.Deaths - previousDay.Deaths,
+                Infected = currentDay.Infected - previousDay.Infected
+            };
+
+            return day;
         }
 
         private static DailyStats MapToDailyStats(DailyPdfStats currentData)
@@ -94,15 +126,55 @@ namespace Code4Ro.CoViz19.Api.Handlers
             {
                 response.DatePublished = currentPdfData.CurrentDayStats.ParsedOn;
                 response.DatePublishedString = currentPdfData.CurrentDayStats.ParsedOnString;
-                var numberOfChildren = currentPdfData.CurrentDayStats.percentageOfChildren;
+                var percentageOfChildren = currentPdfData.CurrentDayStats.PercentageOfChildren;
                 var percentageOfMen = currentPdfData.CurrentDayStats.PercentageOfMen;
                 var percentageOfWomen = currentPdfData.CurrentDayStats.PercentageOfWomen;
 
-                response.Children = numberOfChildren;
-                response.Men = percentageOfMen;
-                response.Women = percentageOfWomen;
-                response.Total = currentPdfData.CurrentDayStats?.DistributionByAge?.Sum(x => x.Value) ?? 0;
+                response.PercentageOfChildren = percentageOfChildren;
+                response.PercentageOfMen = percentageOfMen;
+                response.PercentageOfWomen = percentageOfWomen;
+                response.TotalPercentage = percentageOfChildren + percentageOfWomen + percentageOfMen;
+                response.TotalNumber = currentPdfData.CurrentDayStats.NumberInfected;
             }
+
+            return response;
+        }
+
+        public async Task<QuickStatsV2Model> Handle(GetQuickstatsV2Data request, CancellationToken cancellationToken)
+        {
+            var currentPdfData = await _dataService.GetCurrentPdfData();
+            var response = new QuickStatsV2Model()
+            {
+                History = new InfectionsStatsV2Model[0],
+                Totals = new InfectionsStatsV2Model()
+            };
+            if (currentPdfData?.CurrentDayStats == null)
+            {
+                return response;
+            }
+
+            response.Totals = MapToInfectionsStatsV2Model(currentPdfData.CurrentDayStats);
+
+            var history = currentPdfData.HistoricalData?
+                     .Select(x => x.Value)
+                     .Select(MapToInfectionsStatsV2Model)
+                     .ToList() ?? new List<InfectionsStatsV2Model>();
+
+            history.Add(response.Totals);
+
+            response.History = history.OrderBy(x => x.Date).Skip(1).ToArray();
+
+            return response;
+        }
+
+        private InfectionsStatsV2Model MapToInfectionsStatsV2Model(DailyPdfStats data)
+        {
+            var response = new InfectionsStatsV2Model();
+            response.DateString = data.ParsedOnString;
+            response.Date = data.ParsedOn;
+            response.Cured = data.NumberCured;
+            response.Deaths = data.NumberDeceased;
+            response.Confirmed = data.NumberInfected;
 
             return response;
         }
