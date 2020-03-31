d172c5ff8b6c6861e0fcce8870f95a264f3dbe07
Cristi Habliuc <cristi.habliuc@gmail.com>
2020-03-31 19:18:38 +0300

    updated daily chart legend + age group chart

--- frontend/src/components/cards/age/age.jsx
+++ frontend/src/components/cards/age/age.jsx
@@ -34,7 +34,7 @@ export class AgeCard extends React.PureComponent {
       tooltip: {
         trigger: 'axis',
         axisPointer: {
-          axis: 'x'
+          axis: 'y'
         },
         formatter: (rawData) => {
           const item = rawData[0]
@@ -42,13 +42,14 @@ export class AgeCard extends React.PureComponent {
         },
       },
       legend: {
-        data: labels[0],
-        bottom: '0px',
-        icon: 'circle',
+        show: false,
+        // data: labels[0],
+        // bottom: '0px',
+        // icon: 'circle',
       },
       grid: {
         left: '1%',
-        right: 0,
+        right: '2%',
         bottom: '0',
         top: '2%',
         containLabel: true
@@ -59,7 +60,17 @@ export class AgeCard extends React.PureComponent {
           name: labels[0],
           stack: 'one',
           type: 'bar',
-          color: Constants.confirmedColor
+          color: Constants.confirmedColor,
+          label: {
+            show: true,
+            position: 'right',
+            formatter: (rawData) => {
+              const item = rawData
+              return ` ${item.data.value} (${item.data.percentage}%) `
+            },
+            color: 'black',
+            fontWeight: 'bold'
+          }
         },
       ]
     }
