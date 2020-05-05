92fd6b17d1d22fceddfbffb1a70b78b5904414a2
Andrei Ioniță <hi@andrei.io>
2020-05-05 17:21:37 +0100

    Cleanup

--- frontend/src/config/globals.jsx
+++ frontend/src/config/globals.jsx
@@ -1,33 +1,33 @@
 export const Constants = {
   api: process.env.REACT_APP_API_URL,
 
-  womenColor: "#F77EB9",
-  menColor: "#7EBCFF",
-  childrenColor: "#bbbbbb",
-  unknownColor: "#9FB3C7",
+  womenColor: '#F77EB9',
+  menColor: '#7EBCFF',
+  childrenColor: '#bbbbbb',
+  unknownColor: '#9FB3C7',
 
-  symptomaticColor: "#F8AF69",
-  confirmedColor: "#66A4FB",
-  curedColor: "#65E0E0",
+  symptomaticColor: '#F8AF69',
+  confirmedColor: '#66A4FB',
+  curedColor: '#65E0E0',
 
-  countyLowestColor: "#FFFF66",
-  countyHighestColor: "#DC143C",
+  countyLowestColor: '#FFFF66',
+  countyHighestColor: '#DC143C',
 
-  deathColor: "black",
+  deathColor: 'black',
 
-  womenText: "Femei",
-  menText: "Bărbați",
-  childrenText: "Copii < 18 ani",
-  unknownGenderText: "Necunoscuți",
+  womenText: 'Femei',
+  menText: 'Bărbați',
+  childrenText: 'Copii < 18 ani',
+  unknownGenderText: 'Necunoscuți',
 
-  magenta: "#F1B8FF",
-  orange: "#FDC862",
-  green: "#A4E064",
-  grey: "#B2BECE",
-  lightblue: "#A5D7FC",
+  magenta: '#F1B8FF',
+  orange: '#FDC862',
+  green: '#A4E064',
+  grey: '#B2BECE',
+  lightblue: '#A5D7FC',
 
-  oldestColor: "#CB466E",
-  processingColor: "#FFDE9F",
+  oldestColor: '#CB466E',
+  processingColor: '#FFDE9F',
 
   dailyRecordsLimit: 14,
 };
