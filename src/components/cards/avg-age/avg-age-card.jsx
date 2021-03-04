35d0e448a7339f6f231da93339a11b5f36d51a75
Utwo <mihai.legat@gmail.com>
2021-03-04 20:26:46 +0200

    Resolve React import

--- src/components/cards/avg-age/avg-age-card.jsx
+++ src/components/cards/avg-age/avg-age-card.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Card } from '../../layout/card/card';
 import './avg-age-card.css';
 import { formatDate } from '../../../utils/date';
--- src/components/layout/embed-button/embed-button.jsx
+++ src/components/layout/embed-button/embed-button.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import './embed-button.css';
 import Embed from './../../../images/embed.svg';
 import { useToast } from '../toast/withToastProvider';
