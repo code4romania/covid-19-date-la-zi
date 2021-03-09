28c769c1029cb4272ff93de7463cecde312ba3b2
Utwo <mihai.legat@gmail.com>
2021-03-09 17:04:38 +0200

    Add cursor on logo

--- components/layout/default-layout.js
+++ components/layout/default-layout.js
@@ -1,29 +1,31 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from 'next/link'
+import Image from 'next/image'
 import {
   Header,
   DevelopedBy,
   IncubatedBy,
-} from '@code4ro/taskforce-fe-components';
-import FooterWrapper from './footer/footer';
+} from '@code4ro/taskforce-fe-components'
+import FooterWrapper from './footer/footer'
 
 const Logo = () => (
   <Link href="/">
-    <Image
-      src="/images/logo-coviz.svg"
-      width="110"
-      height="30"
-      alt="Covid-19. Ce trebuie să fac?"
-      priority={true}
-    />
+    <a>
+      <Image
+        src="/images/logo-coviz.svg"
+        width="110"
+        height="30"
+        alt="Covid-19. Ce trebuie să fac?"
+        priority={true}
+      />
+    </a>
   </Link>
-);
+)
 
 const MenuItems = [
   <Link href="/about" key="des">
     <a>Despre proiect</a>
   </Link>,
-];
+]
 
 const DefaultLayout = ({ children }) => {
   return (
@@ -34,7 +36,7 @@ const DefaultLayout = ({ children }) => {
       <IncubatedBy />
       <FooterWrapper />
     </>
-  );
-};
+  )
+}
 
-export default DefaultLayout;
+export default DefaultLayout
